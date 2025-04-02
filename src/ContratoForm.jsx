eeebd674b9e2b4165fe1e7b0e90e04c098297a6a
Junior <junior@wl.local>
2025-04-02 17:59:37 -0300

    build local + serve dist folder

--- src/ContratoForm.jsx
+++ src/ContratoForm.jsx
@@ -35,7 +35,7 @@ export default function ContratoForm() {
     };
 
     try {
-      const res = await fetch("https://webhook.lacintradev.tech/webhook/contrato-emiliano", {
+      const res = await fetch("https://n8n.lacintradev.tech/webhook-test/contrato-emiliano", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload)
